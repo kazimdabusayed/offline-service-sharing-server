6f249fab460bcbce7b4c458bb29545ae3f31d808
kazimdabusayed <kazimohammedabusayed@gmail.com>
2023-11-08 05:34:57 +0600

    Add service api's update and delete methods

--- index.js
+++ index.js
@@ -28,32 +28,67 @@ async function run() {
 		await client.connect();
 
 		const userCollection = client.db("offlineServices").collection("users");
-		const serviceCollection = client.db("offlineServices").collection("services");
+		const serviceCollection = client
+			.db("offlineServices")
+			.collection("services");
 
 		//! user relaed apis
-		app.post("/users", async (req, res) => {
+		app.post("/api/v1/users", async (req, res) => {
 			const user = req.body;
 			const result = await userCollection.insertOne(user);
 			res.send(result);
 		});
-		app.get("/users", async (req, res) => {
+		app.get("/api/v1/users", async (req, res) => {
 			const cursor = userCollection.find();
 			const result = await cursor.toArray();
 			res.send(result);
 		});
-		
+
 		//! service relaed apis
-		app.post("/services", async (req, res) => {
+		app.post("/api/v1/services", async (req, res) => {
 			const service = req.body;
 			console.log(service);
 			const result = await serviceCollection.insertOne(service);
 			res.send(result);
 		});
-		app.get("/services", async (req, res) => {
+		app.get("/api/v1/services", async (req, res) => {
 			const cursor = serviceCollection.find();
 			const result = await cursor.toArray();
 			res.send(result);
 		});
+		//update
+		app.get("/api/v1/services/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await serviceCollection.findOne(query);
+			res.send(result);
+		});
+		app.put("/api/v1/services/:id", async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const options = { upsert: true };
+			const updatedservice = req.body;
+			const product = {
+				$set: {
+					name: updatedservice.name,
+					quantity: updatedservice.quantity,
+					supplier: updatedservice.supplier,
+					taste: updatedservice.taste,
+					category: updatedservice.category,
+					details: updatedservice.details,
+					photo: updatedservice.photo,
+				},
+			};
+			const result = await serviceCollection.updateOne(filter, product);
+			res.send(result);
+		});
+		//delete
+		// app.delete("/api/v1/services/:id", async (req, res) => {
+		// 	const id = req.params.id;
+		// 	const query = { _id: new ObjectId(id) };
+		// 	const result = await productCollection.deleteOne(query);
+		// 	res.send(result);
+		// });
 
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
