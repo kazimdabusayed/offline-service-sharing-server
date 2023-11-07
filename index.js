391a530f55a874bc92ca02ac4139f946fe329747
kazimdabusayed <kazimohammedabusayed@gmail.com>
2023-11-08 01:21:51 +0600

    Add user and service relaed apis

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 //midleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 //?mongodb
@@ -26,6 +26,35 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+
+		const userCollection = client.db("offlineServices").collection("users");
+		const serviceCollection = client.db("offlineServices").collection("services");
+
+		//! user relaed apis
+		app.post("/users", async (req, res) => {
+			const user = req.body;
+			const result = await userCollection.insertOne(user);
+			res.send(result);
+		});
+		app.get("/users", async (req, res) => {
+			const cursor = userCollection.find();
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+		
+		//! service relaed apis
+		app.post("/services", async (req, res) => {
+			const service = req.body;
+			console.log(service);
+			const result = await serviceCollection.insertOne(service);
+			res.send(result);
+		});
+		app.get("/services", async (req, res) => {
+			const cursor = serviceCollection.find();
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log(
